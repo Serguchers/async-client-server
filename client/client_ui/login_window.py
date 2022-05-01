ff754901861be002981756c7cdb422c042f3967f
Sergei <serguchers@gmail.com>
2022-05-01 16:16:55 +0300

    fixes

--- client/client_ui/login_window.py
+++ client/client_ui/login_window.py
@@ -81,7 +81,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "Welcome"))
         self.username.setPlaceholderText(_translate("Form", "Username"))
         self.password.setPlaceholderText(_translate("Form", "Password"))
         self.login_btn.setText(_translate("Form", "Login"))
